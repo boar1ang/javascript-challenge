1a9b02ffa3e42ae14f0c3aae31282b941d421116
boar1ang <inq.ops@gmail.com>
2020-03-17 17:25:50 -0500

    Fourth round revisions.

--- UFO-level-1/static/index.js
+++ UFO-level-1/static/index.js
@@ -8,7 +8,7 @@ console.log(tableData);
 var table = d3.select("#ufo-table");
 var tbody = d3.select("tbody");
 
-// Function to add table data ----------------------
+// Function to add the table data ----------------------
 function populateTable(tableData) {
     tbody.innerHTML = ("");
     tableData.forEach((datum) => {
@@ -20,35 +20,52 @@ function populateTable(tableData) {
         cell.text(value);
     });
 });
-} 
+}
 populateTable(tableData);
-cell.text.toUpperCase();
-// ------------------------------------------------
 
+// Reference the filter button --------------------
 var filterButton = d3.select("#filter-btn");
-var inputElement = d3.select("#datetime");
-
-var resetButton = d3.select("#reset-btn");
-var inputElement = d3.select("#city");
-
+var inputElement0 = d3.select("#datetime"); 
+var inputElement1 = d3.select("#city");
+var inputElement2 = d3.select("#state");
 // -------------------------------------------------
-filterButton.on("click", function() {
-    d3.event.preventDefault();
-    tbody.HTML("");
-    
-    var inputValue = inputElement.property("value");
-        console.log(inputElement);
-        console.log(`Value entered was : ${inputValue}`);
 
-    var filteredResults = tableData.filter(datum => tableData.datetime === inputValue);
+filterButton.on("click", function() 
+{
+    d3.event.preventDefault();
+    //clear content in table body
+    tbody.html("");    
+    
+    var inputValue0 = inputElement0.property("value");
+        console.log(`Date value entered was : ${inputValue0}`);
+    var inputValue1 = inputElement1.property("value");
+        console.log(`City value entered was ${inputValue1}`);
+    var inputValue2 = inputElement2.property("value");
+        console.log(`State value entered was ${inputValue2}`);
+   
+    var filteredResults = tableData.filter(datum => datum.datetime === inputValue0 || datum.city === inputValue1 || datum.state === inputValue2);
         console.log(filteredResults);
+        filteredResults.forEach((datum) => {
+            var row = tbody.append("tr");
+                
+        Object.entries(datum).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+        populateTable(filteredResults);
+    }); 
 });
+// -------------------------------------------------
+
+
+//Reference the reset button & add reset/reload function
+var resetButton = d3.select("#reset-btn");
 
 resetButton.on("click", function resetForm () {
-    tbody.HTML("");
+    tbody.html("");
     var form = d3.select("form");
-    form.submit();
-    form.reset();
-    return false;
-
-})
+    window.location.reload();
+    populateTable(tableData);
+    return true;
+});
+// --------------------------------------------------
